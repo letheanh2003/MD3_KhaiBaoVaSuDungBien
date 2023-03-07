37ede11f82064f2a5f7169f78cddbc923ddedf6b
unknown <letheanhbn2003@gmail.com>
2023-03-07 16:18:53 +0700

    khaibaovasudungbien

---
+++ src/KhaiBaoVaSuDungBien.java
@@ -0,0 +1,16 @@
+public class KhaiBaoVaSuDungBien {
+    public static void main(String[] args) {
+        int i = 10; //khai báo biến i kiểu int có giá trị là 10.
+        float f = 25.5f; // khai báo biến f có kiểu là float có giá trị là 20.5.
+        boolean b = true; //khai báo biến b có kiểu boolean có giá trị là b
+        char c = 'a'; // khai báo biến c có kiểu char có giá trị kí tự là a
+        String s = "Hà Nội"; // khai báo biến có kiểu string có giá trị là chuỗi hà nội
+        System.out.println("i =" + i);
+        System.out.println("f =" + f);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        System.out.println("s =" + s);
+
+
+    }
+}
